682a495283c54e65221d16aee0d9dc06aba4d042
Sin Shimozono <una.veritas@me.com>
2013-01-02 22:44:25 +0900

    organized

--- openstm32sw-stm32f4/stm32f4/maple-bootloader/FlashV2.bat
+++
@@ -1,5 +0,0 @@
-set flasher="%ProgramFiles%\STMicroelectronics\STM32 ST-LINK Utility\ST-LINK Utility\ST-LINK_CLI.exe"
-set binary=build\maple_boot.bin
-
-%flasher% -c SWD -P %binary% 0x08000000 -V
-pause
