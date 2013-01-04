384f1a968227c89d43272b5418bec6ed3d2ff018
Sin Shimozono <una.veritas@me.com>
2013-01-04 20:00:52 +0900

    organize

---
+++ F4D_MP3/src/codec/stlib.h
@@ -0,0 +1,9 @@
+#pragma once
+#include "st_stm32f4xx.h"
+#include "st_stm32f4xx_dma.h"
+#include "st_stm32f4xx_i2c.h"
+#include "st_stm32f4xx_gpio.h"
+#include "st_stm32f4xx_rcc.h"
+#include "st_stm32f4xx_spi.h"
+
+#define assert_param(expr) ((void)0)
