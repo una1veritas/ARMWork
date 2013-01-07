e41f2f4ef0dbba79d5ede17257e54335c12ba032
Sin Shimozono <una.veritas@me.com>
2013-01-08 08:17:25 +0900

    fatfs

--- stm32f4-RTC/main.c
+++ stm32f4-RTC/main.c
@@ -9,8 +9,21 @@
 #include "stm32f4xx.h"
 #include "stm32f4_discovery_sdio_sd.h"
 
-#include "ff.h"
-#include "diskio.h"
+/* Exported functions ------------------------------------------------------- */
+void RTC_Config(void);
+void RTC_TimeRegulate(void);
+void RTC_TimeShow(void);
+void RTC_AlarmShow(void);
+uint8_t USART_Scanf(uint32_t value);
+
+#ifdef __GNUC__
+  /* With GCC/RAISONANCE, small printf (option LD Linker->Libraries->Small printf
+     set to 'Yes') calls __io_putchar() */
+  #define PUTCHAR_PROTOTYPE int __io_putchar(int ch)
+#else
+  #define PUTCHAR_PROTOTYPE int fputc(int ch, FILE *f)
+#endif /* __GNUC__ */
+
 
 #define DBG
 
@@ -22,15 +35,9 @@ void GPIO_Configuration(void);
 void USART6_Configuration(void);
 
 //******************************************************************************
-/* Exported functions ------------------------------------------------------- */
 
-#ifdef __GNUC__
-  /* With GCC/RAISONANCE, small printf (option LD Linker->Libraries->Small printf
-     set to 'Yes') calls __io_putchar() */
-  #define PUTCHAR_PROTOTYPE int __io_putchar(int ch)
-#else
-  #define PUTCHAR_PROTOTYPE int fputc(int ch, FILE *f)
-#endif /* __GNUC__ */
+#include "ff.h"
+#include "diskio.h"
 
 FRESULT res;
 FILINFO fno;
@@ -66,6 +73,14 @@ char *dec32(unsigned long i)
 
 //******************************************************************************
 
+RTC_TimeTypeDef RTC_TimeStructure;
+RTC_InitTypeDef RTC_InitStructure;
+RTC_AlarmTypeDef  RTC_AlarmStructure;
+#define RTC_CLOCK_SOURCE_LSE   /* LSE used as RTC source clock */
+/* #define RTC_CLOCK_SOURCE_LSI */ /* LSI used as RTC source clock. The RTC Clock
+                                      may varies due to LSI frequency dispersion. */ 
+__IO uint32_t AsynchPrediv = 0, SynchPrediv = 0;
+
 int main(void)
 {
   /*!< At this stage the microcontroller clock setting is already configured,
@@ -77,6 +92,7 @@ int main(void)
 	RCC_ClocksTypeDef RCC_Clocks;
 
   NVIC_Configuration(); /* Interrupt Config */
+
 
 	
 #ifdef DBG
@@ -93,6 +109,64 @@ int main(void)
 	printf( "SYSCLK = %dl\n", RCC_Clocks.SYSCLK_Frequency);
 	printf( ", HCLK = %dl\n", RCC_Clocks.HCLK_Frequency);
 
+	  /* Output a message on Hyperterminal using printf function */
+  printf("\n\r  *********************** RTC Hardware Calendar Example ***********************\n\r");
+  
+	/*
+  if (RTC_ReadBackupRegister(RTC_BKP_DR0) != 0x32F2)
+  {  
+    // RTC configuration  //
+    RTC_Config();
+
+    // Configure the RTC data register and RTC prescaler //
+    RTC_InitStructure.RTC_AsynchPrediv = AsynchPrediv;
+    RTC_InitStructure.RTC_SynchPrediv = SynchPrediv;
+    RTC_InitStructure.RTC_HourFormat = RTC_HourFormat_24;
+   
+    // Check on RTC init //
+    if (RTC_Init(&RTC_InitStructure) == ERROR)
+    {
+//      printf("\n\r        /!\\***** RTC Prescaler Config failed *******!\\ \n\r");
+    }
+
+    // Configure the time register //
+    RTC_TimeRegulate(); 
+  }
+  else
+  {
+    // Check if the Power On Reset flag is set //
+    if (RCC_GetFlagStatus(RCC_FLAG_PORRST) != RESET)
+    {
+      printf("\r\n Power On Reset occurred....\n\r");
+    }
+    // Check if the Pin Reset flag is set //
+    else if (RCC_GetFlagStatus(RCC_FLAG_PINRST) != RESET)
+    {
+      printf("\r\n External Reset occurred....\n\r");
+    }
+
+    printf("\n\r No need to configure RTC....\n\r");
+    
+    // Enable the PWR clock //
+    RCC_APB1PeriphClockCmd(RCC_APB1Periph_PWR, ENABLE);
+
+    // Allow access to RTC //
+    PWR_BackupAccessCmd(ENABLE);
+
+    // Wait for RTC APB registers synchronisation //
+    RTC_WaitForSynchro();
+
+    // Clear the RTC Alarm Flag //
+    RTC_ClearFlag(RTC_FLAG_ALRAF);
+
+    // Clear the EXTI Line 17 Pending bit (Connected internally to RTC Alarm) //
+    EXTI_ClearITPendingBit(EXTI_Line17);
+
+    // Display the RTC Time and Alarm //
+    RTC_TimeShow();
+    RTC_AlarmShow();
+  }
+	*/
 #endif
 
 	memset(&fs32, 0, sizeof(FATFS));
@@ -456,6 +530,183 @@ uint8_t USART_Scanf(uint32_t value)
 void _sys_exit(int return_code)
 {
 label:  goto label;  /* endless loop */
+}
+
+/**
+  * @brief  Configure the RTC peripheral by selecting the clock source.
+  * @param  None
+  * @retval None
+  */
+void RTC_Config(void)
+{
+  /* Enable the PWR clock */
+  RCC_APB1PeriphClockCmd(RCC_APB1Periph_PWR, ENABLE);
+
+  /* Allow access to RTC */
+  PWR_BackupAccessCmd(ENABLE);
+    
+#if defined (RTC_CLOCK_SOURCE_LSI)  /* LSI used as RTC source clock*/
+/* The RTC Clock may varies due to LSI frequency dispersion. */   
+  /* Enable the LSI OSC */ 
+  RCC_LSICmd(ENABLE);
+
+  /* Wait till LSI is ready */  
+  while(RCC_GetFlagStatus(RCC_FLAG_LSIRDY) == RESET)
+  {
+  }
+
+  /* Select the RTC Clock Source */
+  RCC_RTCCLKConfig(RCC_RTCCLKSource_LSI);
+  
+  SynchPrediv = 0xFF;
+  AsynchPrediv = 0x7F;
+
+#elif defined (RTC_CLOCK_SOURCE_LSE) /* LSE used as RTC source clock */
+  /* Enable the LSE OSC */
+  RCC_LSEConfig(RCC_LSE_ON);
+
+  /* Wait till LSE is ready */  
+  while(RCC_GetFlagStatus(RCC_FLAG_LSERDY) == RESET)
+  {
+  }
+
+  /* Select the RTC Clock Source */
+  RCC_RTCCLKConfig(RCC_RTCCLKSource_LSE);
+  
+  SynchPrediv = 0xFF;
+  AsynchPrediv = 0x7F;
+
+#else
+  #error Please select the RTC Clock source inside the main.c file
+#endif /* RTC_CLOCK_SOURCE_LSI */
+  
+  /* Enable the RTC Clock */
+  RCC_RTCCLKCmd(ENABLE);
+
+  /* Wait for RTC APB registers synchronisation */
+  RTC_WaitForSynchro();
+}
+
+/**
+  * @brief  Returns the time entered by user, using Hyperterminal.
+  * @param  None
+  * @retval None
+  */
+void RTC_TimeRegulate(void)
+{
+  uint32_t tmp_hh = 0xFF, tmp_mm = 0xFF, tmp_ss = 0xFF;
+
+  printf("\n\r==============Time Settings=====================================\n\r");
+  RTC_TimeStructure.RTC_H12     = RTC_H12_AM;
+  printf("  Please Set Hours:\n\r");
+  while (tmp_hh == 0xFF)
+  {
+    tmp_hh = USART_Scanf(23);
+    RTC_TimeStructure.RTC_Hours = tmp_hh;
+  }
+  printf("  %0.2d\n\r", tmp_hh);
+  
+  printf("  Please Set Minutes:\n\r");
+  while (tmp_mm == 0xFF)
+  {
+    tmp_mm = USART_Scanf(59);
+    RTC_TimeStructure.RTC_Minutes = tmp_mm;
+  }
+  printf("  %0.2d\n\r", tmp_mm);
+  
+  printf("  Please Set Seconds:\n\r");
+  while (tmp_ss == 0xFF)
+  {
+    tmp_ss = USART_Scanf(59);
+    RTC_TimeStructure.RTC_Seconds = tmp_ss;
+  }
+  printf("  %0.2d\n\r", tmp_ss);
+
+  /* Configure the RTC time register */
+  if(RTC_SetTime(RTC_Format_BIN, &RTC_TimeStructure) == ERROR)
+  {
+    printf("\n\r>> !! RTC Set Time failed. !! <<\n\r");
+  } 
+  else
+  {
+    printf("\n\r>> !! RTC Set Time success. !! <<\n\r");
+    RTC_TimeShow();
+    /* Indicator for the RTC configuration */
+    RTC_WriteBackupRegister(RTC_BKP_DR0, 0x32F2);
+  }
+
+  tmp_hh = 0xFF;
+  tmp_mm = 0xFF;
+  tmp_ss = 0xFF;
+
+  /* Disable the Alarm A */
+  RTC_AlarmCmd(RTC_Alarm_A, DISABLE);
+
+  printf("\n\r==============Alarm A Settings=====================================\n\r");
+  RTC_AlarmStructure.RTC_AlarmTime.RTC_H12 = RTC_H12_AM;
+  printf("  Please Set Alarm Hours:\n\r");
+  while (tmp_hh == 0xFF)
+  {
+    tmp_hh = USART_Scanf(23);
+    RTC_AlarmStructure.RTC_AlarmTime.RTC_Hours = tmp_hh;
+  }
+  printf("  %0.2d\n\r", tmp_hh);
+  
+  printf("  Please Set Alarm Minutes:\n\r");
+  while (tmp_mm == 0xFF)
+  {
+    tmp_mm = USART_Scanf(59);
+    RTC_AlarmStructure.RTC_AlarmTime.RTC_Minutes = tmp_mm;
+  }
+  printf("  %0.2d\n\r", tmp_mm);
+  
+  printf("  Please Set Alarm Seconds:\n\r");
+  while (tmp_ss == 0xFF)
+  {
+    tmp_ss = USART_Scanf(59);
+    RTC_AlarmStructure.RTC_AlarmTime.RTC_Seconds = tmp_ss;
+  }
+  printf("  %0.2d", tmp_ss);
+
+  /* Set the Alarm A */
+  RTC_AlarmStructure.RTC_AlarmDateWeekDay = 0x31;
+  RTC_AlarmStructure.RTC_AlarmDateWeekDaySel = RTC_AlarmDateWeekDaySel_Date;
+  RTC_AlarmStructure.RTC_AlarmMask = RTC_AlarmMask_DateWeekDay;
+
+  /* Configure the RTC Alarm A register */
+  RTC_SetAlarm(RTC_Format_BIN, RTC_Alarm_A, &RTC_AlarmStructure);
+  printf("\n\r>> !! RTC Set Alarm success. !! <<\n\r");
+  RTC_AlarmShow();
+
+  /* Enable the RTC Alarm A Interrupt */
+  RTC_ITConfig(RTC_IT_ALRA, ENABLE);
+   
+  /* Enable the alarm  A */
+  RTC_AlarmCmd(RTC_Alarm_A, ENABLE);
+}
+
+/**
+  * @brief  Display the current time on the Hyperterminal.
+  * @param  None
+  * @retval None
+  */
+void RTC_TimeShow(void)
+{
+  /* Get the current Time */
+  RTC_GetTime(RTC_Format_BIN, &RTC_TimeStructure);
+  printf("\n\r  The current time is :  %0.2d:%0.2d:%0.2d \n\r", RTC_TimeStructure.RTC_Hours, RTC_TimeStructure.RTC_Minutes, RTC_TimeStructure.RTC_Seconds);
+}
+
+/**
+  * @brief  Display the current time on the Hyperterminal.
+  * @param  None
+  * @retval None
+  */
+void RTC_AlarmShow(void)
+{
+  /* Get the current Alarm */
+  RTC_GetAlarm(RTC_Format_BIN, RTC_Alarm_A, &RTC_AlarmStructure);
+  printf("\n\r  The current alarm is :  %0.2d:%0.2d:%0.2d \n\r", RTC_AlarmStructure.RTC_AlarmTime.RTC_Hours, RTC_AlarmStructure.RTC_AlarmTime.RTC_Minutes, RTC_AlarmStructure.RTC_AlarmTime.RTC_Seconds);
 }
 
 //******************************************************************************
