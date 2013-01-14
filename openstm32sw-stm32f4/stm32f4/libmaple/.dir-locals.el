8edb0f7a06e8a7532267f5d433cb874bb406762c
Sin Shimozono <una.veritas@me.com>
2013-01-14 17:47:51 +0900

    organized, example folder created

--- openstm32sw-stm32f4/stm32f4/libmaple/.dir-locals.el
+++
@@ -1,6 +0,0 @@
-((c-mode . ((c-basic-offset . 4)
-            (indent-tabs-mode . nil)
-            (tab-width . 50)))         ; display tabs badly on purpose
- (c++-mode . ((c-basic-offset . 4)
-              (indent-tabs-mode . nil)
-              (tab-width . 50))))      ; display tabs badly on purpose
