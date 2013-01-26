d7df1385ae85f08c263dc8a92012ac854eeceb7f
Sin Shimozono <una.veritas@me.com>
2013-01-26 09:07:12 +0900

    stm32-e

--- GLCD_SSD1289/main.c
+++ GLCD_SSD1289/main.c
@@ -12,12 +12,28 @@ int xold,yold;
 
 int main(void)
 {
+<<<<<<< HEAD
 	int i = 1;
+=======
+<<<<<<< HEAD
+  int i = 1;
+=======
+	int i = 1;
+>>>>>>> origin/win@home
+>>>>>>> origin/master
 
   Delay(0x3FFFFF);
   LCD_Init();
   Delay(0x3FFFFF);
   touch_init();
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+  LCD_Clear(BLACK);
+  LCD_SetTextColor(BLUE);
+  
+=======
+>>>>>>> origin/master
   LCD_Clear(DGRAY);
 
   LCD_SetTextColor(WHITE);
@@ -26,6 +42,10 @@ int main(void)
 	LCD_StringLine(10, 10, "This is a pen.");
   Delay(10000);
 	
+<<<<<<< HEAD
+=======
+>>>>>>> origin/win@home
+>>>>>>> origin/master
   while(1)
   {
     Julia3(240,320,120,160,i);
@@ -33,6 +53,20 @@ int main(void)
     if(i>1000)
       i=0;
   }
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+/*	while(1)
+ {
+   Convert_Pos();
+  Pixel(Pen_Point.X0,Pen_Point.Y0,WHITE); 
+  Pixel(Pen_Point.X0,Pen_Point.Y0+1,WHITE);
+  Pixel(Pen_Point.X0+1,Pen_Point.Y0,WHITE);
+  Pixel(Pen_Point.X0+1,Pen_Point.Y0+1,WHITE);
+ }
+	*/
+=======
+>>>>>>> origin/master
 	/*
 	while(1) {
 		Convert_Pos();
@@ -42,5 +76,9 @@ int main(void)
 		Pixel(Pen_Point.X0+1,Pen_Point.Y0+1,WHITE);
 	}
 */
+<<<<<<< HEAD
+=======
+>>>>>>> origin/win@home
+>>>>>>> origin/master
 }
 
