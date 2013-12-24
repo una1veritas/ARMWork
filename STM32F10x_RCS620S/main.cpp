5e4491538b0755ae59f7a40e7b48bdae35607c7c
Sin Shimozono <una.veritas@me.com>
2013-12-24 10:06:55 +0900

    RCS620S

--- STM32F10x_RCS620S/main.cpp
+++ STM32F10x_RCS620S/main.cpp
@@ -7,40 +7,65 @@
 #include "USARTSerial.h"
 #include "RCS620S.h"
 
-usart usart1 = { USART1, PA10, PA9, 115200, WORDLENGTH_8BIT | FLOW_NONE | PARITY_NONE | STOPBITS_1 };
+usart usart1 = { USART1, PA10, PA9, 57600, WORDLENGTH_8BIT | FLOW_NONE | PARITY_NONE | STOPBITS_1 };
 USARTSerial Serial1(&usart1);
 RCS620S nfcreader(Serial1);
 
+#define COMMAND_TIMEOUT 400
+#define POLLING_INTERVAL 500
+ 
 int main(void) {
 	int16_t c;
 	uint32_t since;
+	uint16_t reslen;
+	uint8_t tmp[64];
+	
+	int i;
 	
 	armcmx_init();
 	
 	Serial1.begin(115200);
 
-	usart_begin(&stdserial, 115200);
-	usart_print(&stdserial, "Hello, welcome to the world of F1!\n");
+	Serial.begin(57600);
+	Serial << "Hello, welcome to the world of F1!\n";
 	
-	nfcreader.start();
+	if ( !nfcreader.start() ) {
+		Serial << "RCS620S start failed.\n";
+		while (1);
+	}
 	
 	pinMode(USERLED1, OUTPUT | MEDSPEED);
 	pinMode(USERLED2, OUTPUT | MEDSPEED);
+	
 	since = millis();
 	while(1) {
 		if ( usart_available(&stdserial) > 0 ) {
 			while ( (c = usart_read(&stdserial)) > 0 ) 
 				usart_write(&stdserial, toupper(c&0xff));
 		}
-		if ( since + 500 < millis() ) {
-			if ( digitalRead(USERLED1) ) {
-				digitalWrite(USERLED1, LOW);
-				digitalWrite(USERLED2, HIGH);
+		
+		if ( since + 5000 < millis() ) {
+				nfcreader.timeout = COMMAND_TIMEOUT;
+				reslen = nfcreader.listPassiveTarget(tmp);
+		 
+			if(reslen) {
+				Serial.print("IDm: ");
+				//lcd.print("IDm:");
+				//lcd.setCursor(0, 1);
+				for(i = 0; i < 8; i++)
+				{
+					if(nfcreader.idm[i] / 0x10 == 0) 
+						Serial.print(0); //lcd.print(0);
+					//lcd.print(rcs620s.idm[i], HEX);
+					Serial.print(nfcreader.idm[i], HEX);
+				}
 			} else {
-				digitalWrite(USERLED1, HIGH);
-				digitalWrite(USERLED2, LOW);
+				Serial.print("polling...");
+		//    lcd.print("Polling...");
 			}
-			since = millis();
+		 
+			nfcreader.RFOff();
+			delay(POLLING_INTERVAL);
 		}
 	}
 }
